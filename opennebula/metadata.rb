4861026bb3717667ca715a012accc39bb64c6647
Aaron Baer <aaron@slyness.org>
2011-04-29 23:57:26 -0700

    Release 0.1.5

--- opennebula/metadata.rb
+++ opennebula/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "aaron@slyness.org"
 license          "Apache 2.0"
 description      "Installs/Configures opennebula"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.4"
+version          "0.1.5"
 
 depends "openssl"
 supports "ubuntu", "= 11.04" 
